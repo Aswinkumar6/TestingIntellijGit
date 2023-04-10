99b59b178ccf6471776c23ead5edb03c7f68e904
Aswin Kumar <asandala@arlo.com>
2023-04-10 22:59:24 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    private static final String REGEX = "/(users|user)/[A-Za-z0-9]+/";
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String url = "http://www.example.com/users/john123/";
+        preHandle(url);
+       // Output: http://www.example.com/users/jane456/
+
+    }
+    public static void preHandle(String url){
+        String newUsername = "jane456";
+        String newUrl = url.replaceAll(REGEX, "/$1/" + newUsername + "/");
+        System.out.println(newUrl);
+    }
+
+}
